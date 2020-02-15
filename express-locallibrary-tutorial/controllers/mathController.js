8f49f04ea5a6996d34232caab2430c2ef25308c6
Oliver Clare <oliver.clare@gmail.com>
2020-02-15 12:42:07 +0000

    So close!

--- express-locallibrary-tutorial/controllers/mathController.js
+++ express-locallibrary-tutorial/controllers/mathController.js
@@ -1,13 +1,17 @@
+var Math = require('../models/math');
+
 var async = require("async");
 
 exports.index = function(req, res) {
   async.parallel(
     {
-      questions: function (callback) {
-        callback(null, [
-            {index: 1, question: "What is 1+1?", answer: 2},
-            {index: 2, question: "What is 2+2?", answer: 4}
-        ]); 
+      question_count: function(callback) {
+        Math.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+      },
+      question_list: function(callback) {
+        Math.find({}, "question _id").exec(function(err, list_questions) {
+          callback(err, list_questions);
+        });
       }
     },
     function(err, results) {
--- express-locallibrary-tutorial/models/math.js
+++ express-locallibrary-tutorial/models/math.js
@@ -3,15 +3,31 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var MathSchema = new Schema({
-  _id: { type: Guid, required: true },
-  operand_a: { type: Decimal128, required: true },
-  operand_b: { type: Decimal128, required: true },
+  _id: { type: String, required: true },
+  operand_a: { type: mongoose.Types.Decimal128, required: true },
+  operand_b: { type: mongoose.Types.Decimal128, required: true },
   operator: { type: String, required: true }
 });
 
 // Virtual for question's text
 MathSchema.virtual("question").get(function() {
   return "What is " + this.operand_a + this.operator + this.operand_b + "?";
+});
+
+// Virtual for this question's answer
+MathSchema.virtual("answer").get(function() {
+  var answer;
+  switch (this.operator) {
+    case "+":
+      answer = this.operand_a + this.operand_b;
+      break;
+    case "-":
+      answer = this.operand_a - this.operand_b;
+      break;
+    default:
+      answer = "Unsupported operator: " + this.operator;
+  }
+  return answer;
 });
 
 //Export model
