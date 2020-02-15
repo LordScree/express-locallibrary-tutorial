00aaa35ab280910db5c046d0965b44ba2d040d95
Oliver Clare <oliver.clare@gmail.com>
2020-02-15 12:06:53 +0000

    Fixed a syntax error.

--- express-locallibrary-tutorial/models/math.js
+++ express-locallibrary-tutorial/models/math.js
@@ -6,7 +6,7 @@ var MathSchema = new Schema({
   _id: { type: Guid, required: true },
   operand_a: { type: Decimal128, required: true },
   operand_b: { type: Decimal128, required: true },
-  operator: { type: String, required: true }''
+  operator: { type: String, required: true }
 });
 
 // Virtual for question's text
